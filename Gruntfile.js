9f9bdbf8efa4ea4d56a2c23f61309e079a5e3149
弘树 <tiehang.lth@alibaba-inc.com>
2014-10-14 18:02:14 +0800

    sync daily branch 2.0.2

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
             '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
             '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
             ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
-        buildBase: '2.0/build',
+        buildBase: '2.0.1/build',
         // kmc打包任务，默认情况，入口文件是index.js，可以自行添加入口文件，在files下面
         // 添加
         kmc: {
@@ -22,13 +22,13 @@ module.exports = function(grunt) {
                         path: '../'
                     }
                 ],
-                map: [["<%= pkg.name %>/", "gallery/<%= pkg.name %>/"]]
+                map: [["<%= pkg.name %>/", "kg/<%= pkg.name %>/<%= pkg.version %>/"]]
             },
             main: {
                 files: [
                     {
-                        src: "<%= pkg.version %>/index.js",
-                        dest: "<%= pkg.version %>/build/index.js"
+                        src: "./index.js",
+                        dest: "build/"
                     }
                 ]
             }
@@ -44,21 +44,21 @@ module.exports = function(grunt) {
             },
             base: {
                 files: {
-                    '<%= pkg.version %>/build/index-min.js': ['<%= pkg.version %>/build/index.js']
+                    'build/index-min.js': ['build/index.js']
                 }
             }
         },
         copy: {
             main: {
                 files: [
-                    {src: ['<%= pkg.version %>/**'], dest: '<%= pkg.version %>/build/**'}
+                    {src: ['**'], dest: 'build/**'}
                 ]
             }
         },
         cssmin: {
             combine: {
                 files: {
-                    '<%= pkg.version %>/build/index-min.css': ['<%= pkg.version %>/build/index.css']
+                    'build/index-min.css': ['build/index.css']
                 }
             }
         }
--- README.md
+++ README.md
@@ -1,31 +1,14 @@
 ## musicPlayer
 
-* 版本：2.0
+* 最新版本：2.0.1
 * 概述：musicPlayer是基于Flash和HTML5两个方案开发的MP3音乐播放组件, 3分钟即可完成简单的音乐播放, 我们不需要关心内部封装的数据和控制, 只要专注于侦听数据改变我们的视图即可.
-* 教程：[http://gallery.kissyui.com/musicPlayer/2.0/guide/index.html](http://gallery.kissyui.com/musicPlayer/2.0/guide/index.html)
+* 教程：[http://gallery.kissyui.com/musicPlayer/2.0.1/guide/index.html](http://gallery.kissyui.com/musicPlayer/2.0.1/guide/index.html)
 
 ## demo演示
 
-* [简单的控制音乐播放](http://gallery.kissyui.com/musicPlayer/2.0/guide/demo1.html)
-* [MP3列表模式实例](http://gallery.kissyui.com/musicPlayer/2.0/guide/demo2.html)
-* [完整MP3播放器-演示所有功能](http://gallery.kissyui.com/musicPlayer/2.0/guide/demo3.html)
-
-## FAQ
-1. DEMO无法播放    
-MP3地址失效,请通知到我修改MP3有效地址 / 自行下载DEMO代码修改MP3地址
-
-2. IOS设置 safari浏览器不能播放    
-html audio 在iPhone，ipd,safari浏览器不能播放原因
-(在safri on ios里面明确指出等待用户的交互动作后才能播放media，也就是说如果你没有得到用户的action就播放的话就会被safri拦截)
-
-3. 调试
-修改代码如下
-```javascript    
-var S = KISSY;
-S.Config.debug = true;  //加入如下行
-if (S.Config.debug) {
-var srcPath = "../../../";
-```    
+* [简单的控制音乐播放](http://gallery.kissyui.com/musicPlayer/2.0.1/demo/demo1.html)
+* [MP3列表模式实例](http://gallery.kissyui.com/musicPlayer/2.0.1/demo/demo2.html)
+* [完整MP3播放器-演示所有功能](http://gallery.kissyui.com/musicPlayer/2.0.1/demo/demo3.html)
 
 ## changelog
 
@@ -39,8 +22,6 @@ var srcPath = "../../../";
     [!]支持手机多终端上MP3的播放.
     [!]增加初始始属性'type',可选择强制使用方案.
 
-### V2.0.1 - '修改中'
+### V2.0.1 - '迁移'
 
-    [!]播放器加入对HTML5的支持,自动优先使用audio功能.
-    [!]支持手机多终端上MP3的播放.
-    [!]增加初始始属性'type',可选择强制使用方案.
+    [!]从GITHUB迁移到GITLAB
